59d3963fc243fdf61325df03e52ae82f3c01e177
MgKyaw <mgkyaw20112@gmail.com>
2025-07-17 21:49:23 +0700

    feat: Bind to the template event

--- EventHandling/app.ts
+++ EventHandling/app.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <section>
+    <section (mouseover)="onMouseOver()">
       There's a secret message for you, hover to reveal 👀
       {{ message }}
     </section>
