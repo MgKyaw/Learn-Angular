6d7cf1725f662ac822c0947f50d78c6ab52ca70d
MgKyaw <mgkyaw20112@gmail.com>
2025-07-21 20:58:41 +0700

    feat: Create an input field

--- Forms/user.ts
+++ Forms/user.ts
@@ -1,15 +1,18 @@
-import {Component} from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-user',
+  selector: "app-user",
   template: `
     <p>Username: {{ username }}</p>
     <p>{{ username }}'s favorite framework: {{ favoriteFramework }}</p>
-    <label for="framework">Favorite Framework:</label>
+    <label for="framework">
+      Favorite Framework:
+      <input id="framework" type="text" />
+    </label>
   `,
   imports: [],
 })
 export class User {
-  username = 'youngTech';
-  favoriteFramework = '';
+  username = "youngTech";
+  favoriteFramework = "";
 }
